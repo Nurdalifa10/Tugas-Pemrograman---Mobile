52f5ebbf812da890b43cdd19ecdc125989d23e5d
Fauzan205 <fauzanmansyur806@gmail.com>
2025-08-31 16:38:46 +0900

    Tugas 1 Pemrograman - Mobile

---
+++ tugas_pemrograman_mobile/test/tugas_pemrograman_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_pemrograman_mobile/tugas_pemrograman_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
