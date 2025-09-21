bc8030bbd6a6e282fd7cbc01f080666ce12dbd4c
Nurdalifa10 <nurdalifamarzuki05@gmail.com>
2025-09-22 04:19:48 +0900

    Update Tugas Pemrograman-Mobile

--- tugas_pemrograman_mobile/bin/tugas_pemrograman_mobile.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tugas_pemrograman_mobile/tugas_pemrograman_mobile.dart' as tugas_pemrograman_mobile;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tugas_pemrograman_mobile.calculate()}!');
-}
--- tugas_pemrograman_mobile/test/tugas_pemrograman_mobile_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tugas_pemrograman_mobile/tugas_pemrograman_mobile.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
